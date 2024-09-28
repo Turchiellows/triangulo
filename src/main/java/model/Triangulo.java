1512e820fde40c935252c55ac7a35dad5acff72f
Turchiellows <>
2024-09-28 14:43:10 -0300

    refactor: Mudança de indentação

--- src/main/java/model/Triangulo.java
+++ src/main/java/model/Triangulo.java
@@ -1,59 +1,49 @@
+// PACKAGES
 package model;
 
 public class Triangulo {
-	// ATRIBUTES
-	private double base;
-	private double altura;
+   // ATRIBUTES
 
-	
-	
-	
-	
-	// CONSTGRUCTOR
-	public Triangulo() {
-		this(0, 0);
-	}
+   private double base;
+   private double altura;
 
-	public Triangulo(double base, double altura) {
-		this.base	= base;
-		this.altura	= altura;
-	}
+   // CONSTGRUCTOR
+   public Triangulo() {
+      this(0, 0);
+   }
 
-	
+   public Triangulo(double base, double altura) {
+      this.base = base;
+      this.altura = altura;
+   }
 
+   // Geters and seters
+   public double getBase() {
+      return base;
+   }
 
-	
-	// Geters and seters
-	public double getBase() {
-		return base;
-	}
+   public double getAltura() {
+      return altura;
+   }
 
-	public double getAltura() {
-		return altura;
-	}
-	
-	public void setBase(double base) {
-		if (base < 0) {
-			base = 1;
-		}
-		
-		this.base = base;
-	}
+   public void setBase(double base) {
+      if (base < 0) {
+         base = 1;
+      }
 
-	public void setAltura(double altura) {
-		if (altura < 0) {
-			altura = 1;
-		}
-		this.altura = altura;
-	}
-	
-	
-	
-	
-		
-	// CALCULATE THE AREA
-	public double getArea() {
-		return (getBase() * getAltura()) / 2;
-	}
+      this.base = base;
+   }
+
+   public void setAltura(double altura) {
+      if (altura < 0) {
+         altura = 1;
+      }
+      this.altura = altura;
+   }
+
+   // CALCULATE THE AREA
+   public double getArea() {
+      return (getBase() * getAltura()) / 2;
+   }
 
 }
--- src/main/java/principal/Principal.java
+++ src/main/java/principal/Principal.java
@@ -1,25 +1,22 @@
 // PACKAGES
 package principal;
 
-
 // IMPORTS
 import model.Triangulo;
 import javax.swing.JOptionPane;
 
-
 public class Principal {
-	public static void main(String[] args) {
-		// INSTAINTIATE
-		Triangulo tri = new Triangulo();
-		
-		
-		// COLECT
-		tri.setBase(Double.parseDouble(JOptionPane.showInputDialog("Base")));
-		tri.setAltura(Double.parseDouble(JOptionPane.showInputDialog("Altura")));
-		
-		
-		// OUTPUT
-		JOptionPane.showMessageDialog(null, "Area " + tri.getArea());
-	}
+
+   public static void main(String[] args) {
+      // INSTAINTIATE
+      Triangulo tri = new Triangulo();
+
+      // COLECT
+      tri.setBase(Double.parseDouble(JOptionPane.showInputDialog("Base")));
+      tri.setAltura(Double.parseDouble(JOptionPane.showInputDialog("Altura")));
+
+      // OUTPUT
+      JOptionPane.showMessageDialog(null, "Area " + tri.getArea());
+   }
 
 }
